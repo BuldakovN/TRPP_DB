4d870df9f054238118e17d39824d15e1990677e2
Nikita <nikitabuldakov@mail.ru>
2022-05-10 23:40:35 +0300

    Add new methods

--- flask_app.py
+++ flask_app.py
@@ -1,3 +1,11 @@
+"""
+#Документация для работы с удаленной БД
+
+Все запросы посылаются по адресу:
+
+    http://buldakovn.pythonanywhere.com/метод
+"""
+
 from flask import Flask, request
 from dao import DAO
 import json
@@ -15,7 +23,7 @@ def index():
 def addStudent():
     """
     ##Добавить студента в таблицу
-    На вход поступает POST на адрес /addStudent запрос в виде json-объекта вида
+    На вход поступает POST на адрес **/addStudent** запрос в виде json-объекта вида
 
         {
             FName: имя студента,
@@ -44,7 +52,7 @@ def addStudent():
 def addMessage():
     """
     ##Добавить новое сообщение
-    На вход поступает POST на адрес /addMessage запрос в виде json-объекта вида
+    На вход поступает POST на адрес **/addMessage** запрос в виде json-объекта вида
 
         {
             Text: текст сообщения,
@@ -78,7 +86,7 @@ def addMessage():
 def newMessagesVk():
     """
     ##Получить новые сообщения для ВК
-    GET запрос на адрес /newMessagesVk
+    GET запрос на адрес **/newMessagesVk**
 
     В качестве ответа возвращается json-объект вида
 
@@ -108,7 +116,7 @@ def newMessagesVk():
 def newMessagesTelegramm():
     """
     ##Получить новые сообщения для Телеграмма
-    GET запрос на адрес /newMessagesTelegramm
+    GET запрос на адрес **/newMessagesTelegramm**
 
     В качестве ответа возвращается json-объект вида
 
@@ -134,11 +142,76 @@ def newMessagesTelegramm():
     return json.dumps(response)
 
 
+
+@app.route('/messagesVk')
+def messagesVk():
+    """
+    ##Получить все сообщения для ВК
+    GET запрос на адрес **/messagesVk**
+
+    В качестве ответа возвращается json-объект вида
+
+        [
+            {
+                Id: id сообщения
+                Text: текст сообщения,
+                Date: дата добавления,
+                IdTarget: id студента из таблицы
+                Sent: флаг отправленного сообщения(0 если не отправлено, иначе 1)
+            },
+        ]
+    """
+    dao = DAO(db_name)
+    messages = dao.getMessagesVkTable()
+    response = []
+    for i in messages:
+        d = {}
+        d['Id'] = i[0]
+        d['Text'] = i[1]
+        d['Date'] = i[2]
+        d['IdTarget'] = i[3]
+        d['Sent'] = i[4]
+        response.append(d.copy())
+    return json.dumps(response)
+
+
+@app.route('/messagesTelegramm')
+def messagesTelegramm():
+    """
+    ##Получить все сообщения для Телеграмма
+    GET запрос на адрес **/messagesTelegramm**
+
+    В качестве ответа возвращается json-объект вида
+
+        [
+            {
+                Id: id сообщения
+                Text: текст сообщения,
+                Date: дата добавления,
+                IdTarget: id студента из таблицы
+                Sent: флаг отправленного сообщения(0 если не отправлено, иначе 1)
+            },
+        ]
+    """
+    dao = DAO(db_name)
+    messages = dao.getMessagesTelegrammTable()
+    response = []
+    for i in messages:
+        d = {}
+        d['Id'] = i[0]
+        d['Text'] = i[1]
+        d['Date'] = i[2]
+        d['IdTarget'] = i[3]
+        d['Sent'] = i[4]
+        response.append(d.copy())
+    return json.dumps(response)
+
+
 @app.route('/sendMessage', methods=['POST'])
 def sendMessage():
     """
     ##Сообщить о том, что сообщение доставлено
-    На вход поступает POST запрос на адрес /sendMessage в виде json-объекта вида
+    На вход поступает POST запрос на адрес **/sendMessage** в виде json-объекта вида
 
         {
             Platform: платформа, с которой отправленно сообщение (Vk или Telegramm),
@@ -164,7 +237,7 @@ def sendMessage():
 def getStudents():
     """
     ##Получить всех студентов
-    На вход поступает POST запрос на адрес /getStudents в виде json-объекта вида
+    На вход поступает POST запрос на адрес **/getStudents** в виде json-объекта вида
 
         [
             {
@@ -196,7 +269,7 @@ def getStudents():
 def getStudentById():
     """
     ## Получить студента по его id
-    На вход поступает POST-запрос на адрес /getStudentById с json-объектов вида
+    На вход поступает POST-запрос на адрес **/getStudentById** с json-объектов вида
 
         {
             Id: id студента
